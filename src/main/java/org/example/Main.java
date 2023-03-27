9542c9d32779e244affdfa7c417c1e53ec0ae3e6
muhammadkodyr <okk972016@gmail.com>
2023-03-27 21:01:02 +0500

    done

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+import org.example.imports.FIlter;
+import org.example.imports.Notebook;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+public class Main {
+    public static void main(String[] args) {
+        Set<Notebook> set = new HashSet<>();
+        set.add(new Notebook("Notebook 1", 8, "Windows10", 80000, "HP"));
+        set.add(new Notebook("Notebook 2", 16, "Windows10", 85000, "Asus"));
+        set.add(new Notebook("Notebook 3", 32, "linux", 85000, "Lenovo"));
+        set.add(new Notebook("Notebook 4", 64, "linux", 90000, "Lenovo"));
+
+        FIlter operation = new FIlter(set);
+        operation.start();
+    }
+}
